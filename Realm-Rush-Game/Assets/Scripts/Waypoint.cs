ab1bd99dc59eb189b686ae2fe69d905fc8390ccc
myxsan <yksl3461@gmail.com>
2022-05-29 16:42:46 +0300

    On mouse click ballista can be added on selected tile

--- Realm-Rush-Game/Assets/Scripts/Waypoint.cs
+++ Realm-Rush-Game/Assets/Scripts/Waypoint.cs
@@ -4,15 +4,14 @@ using UnityEngine;
 
 public class Waypoint : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+[SerializeField] bool isPlaceable;
+[SerializeField] GameObject ballistaPrefab;
+    void OnMouseDown() {
+        if(isPlaceable)
+        {
+            Instantiate(ballistaPrefab, transform.position, Quaternion.identity);
+            Debug.Log("Ballista instantiated at" + transform.name);
+            isPlaceable = false;
+        }
     }
 }
