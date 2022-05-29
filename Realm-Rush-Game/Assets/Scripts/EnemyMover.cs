745b1841696589267ef865cfd096fc066a9f903c
myxsan <yksl3461@gmail.com>
2022-05-29 18:49:17 +0300

    Enemies can find the path automatically

--- Realm-Rush-Game/Assets/Scripts/EnemyMover.cs
+++ Realm-Rush-Game/Assets/Scripts/EnemyMover.cs
@@ -8,10 +8,27 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] [Range(0f , 5f)] float enemySpeed = 1f;
     void Start()
     {
-        StartCoroutine(PrintWaypointName());
+        FindPath();
+        ReturnToStart();
+        StartCoroutine(FollowPath());
     }
 
-    IEnumerator PrintWaypointName()
+    void FindPath()
+    {
+        path.Clear();
+        GameObject[] waypoints = GameObject.FindGameObjectsWithTag("Path");
+
+        foreach(GameObject waypoint in waypoints)
+        {
+            path.Add(waypoint.GetComponent<Waypoint>());
+        }
+    }
+
+    void ReturnToStart()
+    {
+        transform.position = path[0].transform.position;
+    }
+    IEnumerator FollowPath()
     {
         foreach(Waypoint waypoint in path)
         {
@@ -28,5 +45,7 @@ public class EnemyMover : MonoBehaviour
                 yield return new WaitForEndOfFrame();
             }
         }
+
+        Destroy(gameObject);
     }
 }
